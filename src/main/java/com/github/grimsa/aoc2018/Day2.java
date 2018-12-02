5e3c25bc9b8597ed0c2d21d0976e2dfd02f7c800
grimsa <gediminas.rimsa@gmail.com>
2018-12-02 15:08:34 +0200

    Solved part 2 of 2018 Day 2 (quick solution)

--- src/main/java/com/github/grimsa/aoc2018/Day2.java
+++ src/main/java/com/github/grimsa/aoc2018/Day2.java
@@ -1,16 +1,19 @@
 package com.github.grimsa.aoc2018;
 
 import com.github.grimsa.io.ClasspathFileReader;
+import com.google.common.base.Strings;
 
 import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day2 {
     public static void main(final String... args) {
         final var boxIds = new ClasspathFileReader("input/2018/day2.txt").get();
         System.out.println("Part one answer: " + getChecksum(boxIds));
+        System.out.println("Part two answer: " + findPrototypeFabricBoxIdCommonLetters(boxIds));
     }
 
     private static long getChecksum(final List<String> boxIds) {
@@ -33,5 +36,29 @@ public class Day2 {
                         Collectors.groupingBy(Function.identity(), Collectors.counting()),
                         countsByLetter -> Set.copyOf(countsByLetter.values())
                 ));
+    }
+
+    private static String findPrototypeFabricBoxIdCommonLetters(final List<String> boxIds) {
+        for (int i = 0; i < boxIds.size(); i++) {
+            for (int j = i + 1; j < boxIds.size(); j++) {
+                final var one = boxIds.get(i);
+                final var two = boxIds.get(j);
+                if (distance(one, two) == 1) {
+                    return commonPart(one, two);
+                }
+            }
+        }
+
+        return "N/A";
+    }
+
+    private static String commonPart(final String one, final String two) {
+        return Strings.commonPrefix(one, two) + Strings.commonSuffix(one, two);
+    }
+
+    private static long distance(final String one, final String two) {
+        return IntStream.range(0, one.length())
+                .filter(index -> one.charAt(index) != two.charAt(index))
+                .count();
     }
 }
